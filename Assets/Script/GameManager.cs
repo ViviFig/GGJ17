e0ce9f486d93a8b1126924b8a0aafbd3fc550cb5
ALIENWARENB\amirb <amir.basyouni@gmail.com>
2017-01-21 10:53:06 +0100

    GameManagerStatico

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,12 +3,38 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	public enum PlayerStates{
-		 Lev1,
-         Lev2,
-         Lev3
-		 }
-		;
+
+    public static GameManager instance;
+
+    #region Events declaration
+    public delegate void GameEvent();
+    //eventi base del gioco.
+    public event GameEvent OnGameStart;
+    public event GameEvent OnLevelEnd;
+    public event GameEvent OnPlayLevel;
+    public event GameEvent OnGameWin;
+    // evento che fa partire il gioco/livello
+
+    //eventi per i bonus
+    public event GameEvent OnBonusTaken;
+
+    #endregion
+
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+
+        if (instance != this)
+            Destroy(this.gameObject);
+
+    }
+
+    public void HandleOnBonusTaken() { }
+
+    // Update is called once per frame
+    void Update() { }
+
 	public PlayerStates currentPlayerStates;
     
 	
@@ -22,4 +48,10 @@ public class GameManager : MonoBehaviour {
                 break;
         }
 	}
+}
+public enum PlayerStates
+{
+    Lev1,
+    Lev2,
+    Lev3
 }
