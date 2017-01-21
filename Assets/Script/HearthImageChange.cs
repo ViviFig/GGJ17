1b60387d6c9aed0135eafaa59916b2544036723b
ALIENWARENB\amirb <amir.basyouni@gmail.com>
2017-01-21 16:23:17 +0100

    Importato fmod e inizializzato la hud

---
+++ Assets/Script/HearthImageChange.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HearthImageChange : MonoBehaviour {
+
+   [HideInInspector] public Image BackgroundSprite;
+    public HearthImage ActualHearthImage;//TODO   [HideInInspector] 
+
+    public void OnHearthTaken()
+    {
+        switch (ActualHearthImage)
+        {
+            case HearthImage.Empty:
+                BackgroundSprite.sprite = Resources.Load<Sprite>("Empty");
+                break;
+            case HearthImage.Incomplete:
+                BackgroundSprite.sprite = Resources.Load<Sprite>("Incomplete");
+                break;
+            case HearthImage.Half:
+                BackgroundSprite.sprite = Resources.Load<Sprite>("Half");
+                break;
+            case HearthImage.Full:
+                BackgroundSprite.sprite = Resources.Load<Sprite>("Full");
+                break;
+            default:
+                break;
+        }
+    }
+    
+}
+public enum HearthImage{
+    Empty,
+    Incomplete,
+    Half,
+    Full
+}
---
+++ Assets/Script/HudManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HudManager : MonoBehaviour {
+
+    public void OnClickStart() {
+        //Emette un suono
+        //Disabilita l'hud
+    }
+
+    public void OnClickExit() {
+        // Emette solo il suono e chiude il gioco
+    }
+}
